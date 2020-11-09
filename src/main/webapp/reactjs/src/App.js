fd9ecaac41c9e7ef0498f2206771f12ff04ad9f0
el08 <pawliko14@gmail.com>
2020-11-09 12:00:10 +0100

    Adding Reactjs to spring boot project. First configuration first commit

---
+++ src/main/webapp/reactjs/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello React v2
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
