7ff1c38b266ac40769e7c60c79a81d7e02d7b9f5
el08 <pawliko14@gmail.com>
2020-11-09 13:42:06 +0100

    added: default View Footer 3 sub-pages and routes to them

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
 import React from 'react';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import NavigationBar from './components/NavigationBar';
+import {Container,Row,Col} from 'react-bootstrap';
+
+import Welcome from './components/Welcome';
+import Footer from './components/Footer';
+
+import ActiveWorkers from './components/ActiveWorkers';
+import CurrentState from './components/CurrentState';
+import LastActivities from './components/LastActivities';
+
 function App() {
+
+    const marginTop = {
+        marginTop:"20px"
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello React v2
-        </p>
-      </header>
-    </div>
+    <Router>
+        <NavigationBar/>
+        <Container>
+            <Row>
+            <Col lg={12} style={marginTop}>
+                <Switch>
+                    <Route path = "/" exact component={Welcome} />
+                    <Route path = "/test" exact component={CurrentState} />
+                    <Route path = "/test1" exact component={ActiveWorkers} />
+                    <Route path = "/test2" exact component={LastActivities} />
+
+                </Switch>
+             </Col>
+            </Row>
+        </Container>
+        <Footer/>
+    </Router>
   );
 }
 
---
+++ src/main/webapp/reactjs/src/components/CurrentState.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CurrentState extends React.Component {
+    render()
+    {
+        return (
+        <div className="text-white">Get current state of workers</div>
+        );
+    }
+}
+
+export default CurrentState;
